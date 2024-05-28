fd61dc9d6453a314fac0919e065f0d24c38a74e1
mankal-27 <mnkalkutagi@gmail.com>
2024-05-28 11:00:10 +0530

    advJs

---
+++ AdvJs/Ch3_Types/1_strings.js
@@ -0,0 +1,13 @@
+let msg = "Hello, " + "world";
+let greeting = "Welcome to my blog"
+
+let s = "Hello, world";
+//length
+console.log("Length of string : ",s.length);
+//Portion of string
+console.log("Obtain portion of string : ", s.substring(1,3));
+//characters
+console.log("Use Slice : ", s.slice(1,4));
+console.log("Use Slice 1 : ", s.slice(-3));
+//split using delimiter
+console.log("Split : ", s.split(","));
